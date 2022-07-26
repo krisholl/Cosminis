28e3def4366844e4d46b450ca2b2afd6fd743704
unknown <syunoncasual@gmail.com>
2022-07-26 17:33:47 -0400

    Added documentation

--- Cosminis/DataAccess/EntitesModels/Species.cs
+++ Cosminis/DataAccess/EntitesModels/Species.cs
@@ -7,9 +7,16 @@ namespace DataAccess.Entities
     {
         Volcanic, Glacial, Forest, Sky, Holy, Dark
     }
+
     public partial class Species
     { 
-        public string StatusToString(ElementSpecies stringSpecies)                                   
+        /// <summary>
+        /// Takes a ElementSpecies and return the string equivalent of the enumeration
+        /// </summary>
+        /// <param name="elementSpecies"></param>
+        /// <returns>String of the particular element</returns>
+        /// <exception cref="ElementNotFound">Occurs if no element exist matching the given input</exception>
+        public string ElementToString(ElementSpecies elementSpecies)                                   
         {
             Dictionary<ElementSpecies,string> dictElement = new Dictionary<ElementSpecies, string>()
             {
@@ -21,11 +28,12 @@ namespace DataAccess.Entities
                 {ElementSpecies.Dark, "Dark"}
             };
 
-            if(dictElement.ContainsKey(stringSpecies))
+            if(dictElement.ContainsKey(elementSpecies))
             {
-                return dictElement[stringSpecies];
+                return dictElement[elementSpecies];
             };
             return "NotFound";
         }
+
     }
 }
