d35198e1635cb8ca6577623117e7d42eb8f2663b
David <dave22011@yahoo.com>
2022-07-27 19:12:50 -0400

    Finished companion partial class and companion models

--- Cosminis/DataAccess/EntitesModels/Companion.cs
+++ Cosminis/DataAccess/EntitesModels/Companion.cs
@@ -1,17 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using Models;
 
 namespace DataAccess.Entities
 {
-    public enum MoodCompanion                            
-    {
-        Happy, Sad, Angry, Tired, Anxious, Excited, Chill
-    }
     public partial class Companion
-    {
+    { 
+        public Companion(Models.CompanionMoodWithEnum Companion)
+        {
+            this.CreatureId = Companion.CreatureId;
+            this.UserFk = Companion.UserFk;
+            this.SpeciesFk = Companion.SpeciesFk;
+            this.Nickname = Companion.Nickname;
+            this.Mood = Companion.MoodToString(Companion.Mood);
+            this.Hunger = Companion.Hunger;
+        }
+
         public override string ToString()
         { 
-            return $"CreatureId: {this.CreatureId}, Nickname: {this.Nickname}, Mood: {this.Mood}, Hunger: {this.Hunger}";
+            return 
+                $"CreatureId: {this.CreatureId}, " + 
+                $"Nickname: {this.Nickname}, " + 
+                $"Mood: {this.Mood}, " + 
+                $"Hunger: {this.Hunger}"; 
         }  
     }
 }
+
+
+
+
+
+
+
+   
+   
---
+++ Cosminis/Models/Companion.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+
+namespace Models;
+
+    public enum MoodCompanion                            
+    {
+        Happy, Sad, Angry, Tired, Anxious, Excited, Chill
+    }
+
+    public class CompanionMoodWithEnum
+    { 
+        public int CreatureId { get; set; }
+        public int UserFk { get; set; }
+        public int SpeciesFk { get; set; }
+        public string? Nickname { get; set; }
+        public MoodCompanion Mood { get; set; }
+        public int? Hunger { get; set; }
+
+        public CompanionMoodWithEnum()
+        {
+            
+        }
+       
+        public string MoodToString(MoodCompanion Mood)                                   
+        {
+            Dictionary<MoodCompanion, string> dictMood = new Dictionary<MoodCompanion, string>()
+            {
+                {MoodCompanion.Happy, "Happy"},
+                {MoodCompanion.Sad, "Sad"},
+                {MoodCompanion.Angry, "Angry"},
+                {MoodCompanion.Tired, "Tired"},
+                {MoodCompanion.Anxious, "Anxious"},
+                {MoodCompanion.Excited, "Excited"},
+                {MoodCompanion.Chill, "Chill"}
+
+            };
+
+            if(dictMood.ContainsKey(Mood))
+            {
+                return dictMood[Mood];
+            }
+            return "NotFound";
+        }
+
+        public override string ToString()
+        { 
+            return 
+                $"CreatureId: {this.CreatureId}, " + 
+                $"Nickname: {this.Nickname}, " + 
+                $"Mood: {this.Mood}, " + 
+                $"Hunger: {this.Hunger}"; 
+        }  
+    }
