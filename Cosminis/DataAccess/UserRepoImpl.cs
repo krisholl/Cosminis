7e6ff26e940934c54fb2ff0b7dee242f6ca9f070
TheFakeLorLyons <lorelailyons@protonmail.com>
2022-07-28 17:31:58 -0400

    Testing merging different but the same files.

---
+++ Cosminis/DataAccess/UserRepoImpl.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess;
+using Exceptions;
+using Models;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccess
+{
+    public class UserRepository
+    {
+        /// <summary>
+        /// Retrieves and displays a list of all users contained in the users table of WALS_P2 database.
+        /// </summary>
+        /// <param userName="userName"></param>
+        /// <returns>returns(user list)</returns>
+        /// <exception cref="RecordNotFoundException">exception descriptions</exception>
+        public User GetUserByUserName()
+        {
+            
+        }
+    }
+}
