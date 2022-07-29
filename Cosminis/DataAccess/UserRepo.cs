5086b26676cfd4515d69bf2fe752f78067e418e7
David <dave22011@yahoo.com>
2022-07-29 17:02:11 -0400

    Updated UserRepo

--- Cosminis/DataAccess/UserRepo.cs
+++ Cosminis/DataAccess/UserRepo.cs
@@ -4,23 +4,35 @@ using Models;
 using System;
 using System.Data;
 using System.Data.SqlClient;
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess;
  
 public class UserRepo
 {
-    public string testString = "Testing Merge";
-    
-    /// <summary>
-    /// Retrieves and displays a list of all users contained in the users table of WALS_P2 database.
-    /// </summary>
-    /// <param userName="userName"></param>
-    /// <returns>returns(user list)</returns>
-    /// <exception cref="RecordNotFoundException">exception descriptions</exception>
-    public User GetUserByUserName()
+    private readonly DbContext _context;
+
+    public UserRepo(DbContext context)
     {
-        throw new ResourceNotFound();
+        _context = context;
     }
 
-    public string StinkyPooPoo = "Pudding";
+    public User CreateUser(User user)
+    {
+        _context.Add(user);
+
+        _context.SaveChanges();
+
+        _context.ChangeTracker.Clear();
+
+        return user;
+    }
+  
+   /* public User GetUserByUserName()
+    {
+        throw new ResourceNotFound();
+    }*/
+
+   
 }
