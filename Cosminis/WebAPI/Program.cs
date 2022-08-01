f3f85e42567946c9d625729a8dbcebf942310863
TheFakeLorLyons <lorelailyons@protonmail.com>
2022-08-01 17:56:10 -0400

    fixed pushed CS.

--- Cosminis/WebAPI/Program.cs
+++ Cosminis/WebAPI/Program.cs
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<wearelosingsteamContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Server=tcp:p2dbs.database.windows.net,1433;Initial Catalog=wearelosingsteam;Persist Security Info=False;User ID=wearelosingsteam;Password=weL0stSteam;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;")));
+builder.Services.AddDbContext<wearelosingsteamContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("")));
 builder.Services.AddScoped<User>();
 builder.Services.AddScoped<UserRepo>();
 
