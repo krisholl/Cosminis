198d6d18a780b804bd05ec518edf8b19802ea8cb
unknown <syunoncasual@gmail.com>
2022-08-01 16:52:40 -0400

    hopefully this works

--- Cosminis/WebAPI/Program.cs
+++ Cosminis/WebAPI/Program.cs
@@ -1,6 +1,30 @@
+using DataAccess.Entities;
+using CustomExceptions;
+using Models;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<wearelosingsteamContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString()));
+builder.Services.AddScoped<User>();
+builder.Services.AddScoped<UserRepo>();
+
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/createUser", (User user, UserRepo repo) => 
+{
+	return repo.CreateUser(user);
+});
 
 app.Run();
