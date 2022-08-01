a85d4fc748fbceace439604eb5c7b8a9b52afff6
TheFakeLorLyons <lorelailyons@protonmail.com>
2022-08-01 18:57:22 -0400

    Trying to connect to DB.

--- Cosminis/DataAccess/CompanionRepo.cs
+++ Cosminis/DataAccess/CompanionRepo.cs
@@ -4,35 +4,46 @@ using Models;
 using System;
 using System.Data;
 using System.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess;
  
 public class CompanionRepo// : ICompanionDAO
 {
-    /// <summary>
-    /// Generates a companion for the user. If the user is a new user, the companion will be generated from an egg one minute after the egg has been obtained.
-    /// Otherwise any eggs will hatch 72 hours after they have been generated.
-    /// </summary>
-    /// <param randomCreature="Name for the random number generator itself."></param>
-    /// <param creatureRoulette="Integer stores number of ^randomCreature as a result for generating a specific companion."></param>
-    /// <returns>Will create and add a companion into the users inventory.</returns>
-    /// <exception cref="Exception">exception descriptions</exception>
-    public Companion GenerateCompanion(Companion newCompanion)
+    private readonly wearelosingsteamContext _context;
+
+    public CompanionRepo(wearelosingsteamContext context)
     {
-        Random randomCreature = new Random();
-        int creatureRoulette = randomCreature.Next(6);
-
-        //MoodMethod();
-
-        throw new Exception();
+        _context = context;
     }
 
     /// <summary>
     /// Generates a companion for the user. If the user is a new user, the companion will be generated from an egg one minute after the egg has been obtained.
     /// Otherwise any eggs will hatch 72 hours after they have been generated.
     /// </summary>
-    /// <param randomCreature="Name for the random number generator itself."></param>
-    /// <param creatureRoulette="Integer stores number of ^randomCreature as a result for generating a specific companion."></param>
+    /// <param newCompanion="New companion to be generated."></param>
+    /// <returns>Will create and add a companion into the users inventory.</returns>
+    /// <exception cref="Exception">exception descriptions</exception>
+    /*
+    public Companion GenerateCompanion(Companion newCompanion)
+    {
+        Random randomCreature = new Random();
+        int creatureRoulette = randomCreature.Next(6);
+
+        User newUser = new User();
+
+        if(newUser.Companion)
+
+        //MoodMethod();
+
+        throw new Exception();
+    }
+    */
+    /// <summary>
+    /// Generates a companion for the user. If the user is a new user, the companion will be generated from an egg one minute after the egg has been obtained.
+    /// Otherwise any eggs will hatch 72 hours after they have been generated.
+    /// </summary>
+    /// <param mood="Daily mood of the companion"></param>
     /// <returns>Will create and add a companion into the users inventory.</returns>
     /// <exception cref="Exception">exception descriptions</exception>
     public Companion SetCompanionMood(MoodCompanion mood)
@@ -43,5 +54,29 @@ public class CompanionRepo// : ICompanionDAO
 
 
         throw new Exception();
+    }
+
+    /// <summary>
+    /// Generates a companion for the user. If the user is a new user, the companion will be generated from an egg one minute after the egg has been obtained.
+    /// Otherwise any eggs will hatch 72 hours after they have been generated.
+    /// </summary>
+    /// <param mood="Daily mood of the companion"></param>
+    /// <returns>Will create and add a companion into the users inventory.</returns>
+    /// <exception cref="Exception">exception descriptions</exception>    
+    public List<Companion> GetAllCompanions()
+    {
+        return _context.Companions.ToList();                                  
+    }
+
+    /// <summary>
+    /// Generates a companion for the user. If the user is a new user, the companion will be generated from an egg one minute after the egg has been obtained.
+    /// Otherwise any eggs will hatch 72 hours after they have been generated.
+    /// </summary>
+    /// <param mood="Daily mood of the companion"></param>
+    /// <returns>Will create and add a companion into the users inventory.</returns>
+    /// <exception cref="Exception">exception descriptions</exception>    
+    public List<Companion> GetCompanionsByUserId(User username)
+    {
+        return _context.Companions.ToList();                           
     }
 }
