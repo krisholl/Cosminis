f7bf54cf8ca731c9888f486c720fc7f0bec975c1
TheFakeLorLyons <lorelailyons@protonmail.com>
2022-08-04 11:57:01 -0400

    Added endpoints and controllers to the non-gitignored WebAPI folder.

--- Cosminis/WebAPI/Program.cs
+++ Cosminis/WebAPI/Program.cs
@@ -11,9 +11,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddDbContext<wearelosingsteamContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("")));
+builder.Services.AddDbContext<wearelosingsteamContext>(options => options.UseSqlServer());
+builder.Services.AddScoped<CompanionController>();
+builder.Services.AddScoped<CompanionServices>();
 builder.Services.AddScoped<User>();
 builder.Services.AddScoped<UserRepo>();
+builder.Services.AddScoped<ICompanionDAO, CompanionRepo>();
 
 var app = builder.Build();
 
@@ -21,5 +24,15 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/GetAllCompanions", (CompanionController CompControl) => CompControl.GetAllCompanions());
+
+app.MapGet("/companions/SearchByCompanionId", (int companionId, CompanionController CompControl) => CompControl.SearchForCompanionById(companionId));
+
+app.MapGet("/companions/SearchByUserId", (int userId, CompanionController CompControl) => CompControl.SearchForCompanionByUserId(userId));
+
+app.MapPost("/companions/Nickname", (int companionId, string? nickname, CompanionController CompControl) => CompControl.NicknameCompanion(companionId, nickname));
+
+app.MapPost("/companions/GenerateCompanion", (string username, CompanionController CompControl) => CompControl.GenerateCompanion(username));
 
 app.Run();
