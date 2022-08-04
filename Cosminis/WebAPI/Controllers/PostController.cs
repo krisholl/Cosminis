15d04b1f391aa9f35e814ce4bb592d315e25bc70
David <dave22011@yahoo.com>
2022-08-04 01:34:56 -0400

    Added a 409 Conflict and a 201 Created

--- Cosminis/WebAPI/Controllers/PostController.cs
+++ Cosminis/WebAPI/Controllers/PostController.cs
@@ -19,14 +19,19 @@ public class PostController
 
     public IResult SubmitPostResourceGen(Post post)
     { 
+        if (post.Content.Length > 255) 
+        {
+            return Results.Conflict("Posts' content cannot be greater than 255 characters."); //processing the request would create a conflict within the resource
+        }
         try
         {
             Post postInfo = _postServices.SubmitPostResourceGen(post);
-            return Results.Ok(postInfo); 
+            return Results.Created("/submitPost", postInfo);   
         }
         catch(ResourceNotFound)
         {
             return Results.BadRequest("Such a user does not exist"); 
         }   
+
     }
 }
