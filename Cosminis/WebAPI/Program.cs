37ffae7355c9c311727a19a8ba5c91640c73b89f
David <dave22011@yahoo.com>
2022-08-09 05:13:26 -0400

    Finished Comments

--- Cosminis/WebAPI/Program.cs
+++ Cosminis/WebAPI/Program.cs
@@ -9,21 +9,24 @@ using Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<wearelosingsteamContext>(options => options.UseSqlServer());
+builder.Services.AddDbContext<wearelosingsteamContext>(options => options.UseSqlServer("Server=tcp:p2dbs.database.windows.net,1433;Initial Catalog=wearelosingsteam;Persist Security Info=False;User ID=wearelosingsteam;Password=weL0stSteam;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
 builder.Services.AddScoped<ICompanionDAO, CompanionRepo>();
 builder.Services.AddScoped<IUserDAO, UserRepo>();
 builder.Services.AddScoped<IPostDAO, PostRepo>();
+builder.Services.AddScoped<ICommentDAO, CommentRepo>();
 builder.Services.AddScoped<IResourceGen, ResourceRepo>();
 builder.Services.AddScoped<ILikeIt, LikeRepo>();
 
 builder.Services.AddScoped<CompanionServices>();
 builder.Services.AddScoped<UserServices>();
 builder.Services.AddScoped<PostServices>();
+builder.Services.AddScoped<CommentServices>();
 builder.Services.AddScoped<LikeServices>();
 
 builder.Services.AddScoped<CompanionController>();
 builder.Services.AddScoped<UserController>();
 builder.Services.AddScoped<PostController>();
+builder.Services.AddScoped<CommentController>();
 builder.Services.AddScoped<LikeController>();
 
 builder.Services.AddEndpointsApiExplorer();
@@ -53,6 +56,16 @@ app.MapGet("/postsByUser/{username}", (string username, PostController controlle
 	return controller.GetPostsByUsername(username);
 });
 
+app.MapGet("/commentsUnder/{postId}", (int postId, CommentController controller) => 
+{
+	return controller.GetCommentsByPostId(postId);
+});
+
+app.MapDelete("/commentsBy/{commentId}", (int commentId, CommentController controller) => 
+{
+	return controller.RemoveComment(commentId);
+});
+
 /*
 app.MapPost("/createUser", (User user, IUserDAO repo) => //to be replaced by registerUser
 {
@@ -62,6 +75,11 @@ app.MapPost("/createUser", (User user, IUserDAO repo) => //to be replaced by reg
 app.MapPost("/submitPost", (Post post, PostController controller) => 
 {
 	return controller.SubmitPostResourceGen(post);
+});
+
+app.MapPost("/submitComment", (Comment comment, CommentController controller) =>
+{
+	return controller.SubmitCommentResourceGen(comment);
 });
 
 app.MapGet("/GetAllCompanions", (CompanionController CompControl) => CompControl.GetAllCompanions());
