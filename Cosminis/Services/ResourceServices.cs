bda16fde75658e4c6ec303d0ecd3a85c62576ba4
unknown <syunoncasual@gmail.com>
2022-08-17 21:10:12 -0400

    fixed get food by user

--- Cosminis/Services/ResourceServices.cs
+++ Cosminis/Services/ResourceServices.cs
@@ -21,10 +21,6 @@ public class ResourceServices
     public List<FoodInventory> GetFoodInventoryByUserId(int userId)
     {
         List<FoodInventory> food = _resourceRepo.GetFoodInventoryByUserId(userId);
-        if (food.Count < 1) 
-        {
-            throw new ResourceNotFound();
-        }
         return food; 
     }
 
